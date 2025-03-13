3b2626269da15ca727c10270e6901eea749a2ef6
ashutosh <radaashutosh556@gmail.com>
2025-03-13 11:07:59 +0530

    remove commented code in testimonial section

--- src/components/Home/Testimonials/index.tsx
+++ src/components/Home/Testimonials/index.tsx
@@ -30,41 +30,6 @@ const Testimonials: React.FC = () => {
             What our clients says
           </h2>
         </div>
-        {/* <div className='grid grid-cols-12 gap-10'>
-          <div className='col-span-8 content-center'>
-            <div className='flex gap-10'>
-              <div>
-                <Icon
-                  icon={'fluent:home-empty-20-regular'}
-                  width={44}
-                  height={44}
-                  className='text-primary'
-                />
-              </div>
-              <div>
-                <h4 className='text-3xl font-medium text-white max-w-90p leading-12 mb-8'>
-                  I found my ideal home in no time! the listings were detailed,
-                  the photos were accurate, and the whole process felt seamless.
-                  customer service was top-notch, answering all my questions. i
-                  will definitely use this platform again in the future!
-                </h4>
-                <h6 className='text-xm font-medium text-white mb-1'>
-                  Emily & John Smith
-                </h6>
-                <p className='text-base font-normal text-white/40'>Buyer</p>
-              </div>
-            </div>
-          </div>
-          <div className='col-span-4'>
-            <Image
-              src={'/images/testimonial/testimonial1.jpg'}
-              alt='test1'
-              width={439}
-              height={434}
-              className='w-full h-full rounded-2xl'
-            />
-          </div>
-        </div> */}
         <Carousel
           opts={{
             align: 'start',
