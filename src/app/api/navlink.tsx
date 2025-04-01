b63ba20bf878e932d39ba53f4772eabbe99eb697
ashutosh <radaashutosh556@gmail.com>
2025-04-01 12:25:52 +0530

    href added of property list page in navlink (sidebar)

--- src/app/api/navlink.tsx
+++ src/app/api/navlink.tsx
@@ -2,7 +2,7 @@ import { NavLinks } from '@/types/navlink'
 
 export const navLinks: NavLinks[] = [
   { label: 'Home', href: '/' },
-  { label: 'Properties', href: '#' },
+  { label: 'Properties', href: '/PropertyListing' },
   { label: 'Testimonials', href: '#' },
   { label: 'Blog', href: '#' },
   { label: 'Contact', href: '#' },
